859530daebe835361d19c1174cceb644721dfb54
chetandev <chetandev@chetandevs-MacBook-Pro.local>
2017-05-17 18:45:16 +0530

    style chanegs

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,13 +1,10 @@
-
-
-
 graphite.controller('homeController', ['$scope', '$routeParams', '$route', 'data', '$interval', function($scope, $routeParams, $route, data, $interval) {
 
     $scope.serverData;
     var factory = new data();
 
-    $interval(getData, 10000);
-
+     $interval(getData, 10000);
+ 
 
     function getData() {
         factory.get_server_data()
